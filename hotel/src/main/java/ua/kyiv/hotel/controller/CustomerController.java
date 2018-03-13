39b5e4c0c41cebe8ae4d4c784dc84c7739cd35ae
falexsandr <falexsandr@localhost>
2018-03-13 13:05:00 +0200

    to do

--- hotel/src/main/java/ua/kyiv/hotel/controller/CustomerController.java
+++ hotel/src/main/java/ua/kyiv/hotel/controller/CustomerController.java
@@ -11,22 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.kyiv.hotel.dao.CustomerDao;
+import ua.kyiv.hotel.dao.DaoFactory;
 import ua.kyiv.hotel.entites.Customer;
 
 @RestController
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDao customerDao;
+	private DaoFactory daoFactory;
 
 	@RequestMapping("/customer")
 	public ResponseEntity<List<Customer>> all() {
+		CustomerDao customerDao = daoFactory.createCompanyDao();
 		List<Customer> list = customerDao.getAll();
 		return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
 	}
 	
 	@GetMapping("/customer/{id}")
 	public ResponseEntity<Customer> findId(@PathVariable("id") Integer id) {
+		CustomerDao customerDao = daoFactory.createCompanyDao();
 		ResponseEntity<Customer> customer = new ResponseEntity<>(customerDao.byId(id), HttpStatus.OK);;
 		return customer;
 	}
